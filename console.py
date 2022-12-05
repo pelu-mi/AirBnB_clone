514ab4bb05966b0627edee98a15457299b3f99d5
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-05 13:08:42 +0100

    pycodestyle fix

--- console.py
+++ console.py
@@ -46,7 +46,9 @@ class HBNBCommand(cmd.Cmd):
             # Specially handle update with dictionary
             regex_dictionary = re.search('^({.*})$', attributes)
             if regex_dictionary:
-                self.update_dictionary(class_name, user_id, regex_dictionary.group(1))
+                # Shorten class_name for pycodestyle_fix
+                c = class_name
+                self.update_dictionary(c, user_id, regex_dictionary.group(1))
                 return ""
             # Organize all the values into a string: regex_values as re_vals
             re_vals = re.search('^(?:"([^"]*)")?(?:, (.*))?$', attributes)
